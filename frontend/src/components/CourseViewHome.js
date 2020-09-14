a3a88f4f63be6dddc0a9f184e89dcfb5b0090e35
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-09-15 00:54:46 +0600

    Filter Course based on - Category | Level | Module | Instructor added

--- frontend/src/components/CourseViewHome.js
+++ frontend/src/components/CourseViewHome.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   CssBaseline,
   Grid,
@@ -17,6 +17,12 @@ import {
   CardActionArea,
   TextField,
   ButtonGroup,
+  Fade,
+  Avatar,
+  ListItemAvatar,
+  ListItemText,
+  MenuItem,
+  Menu,
 } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
 import axios from "../api/Config";
@@ -27,6 +33,8 @@ import {
   ShopOutlined,
   ShoppingCart,
   FilterList,
+  ExpandMore,
+  ArrowDropDown,
 } from "@material-ui/icons";
 import { useHistory } from "react-router-dom";
 import Navbar from "./Navbar";
@@ -90,12 +98,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const ITEM_HEIGHT = 48;
+
 export default function CourseViewHome(props) {
   const classes = useStyles();
 
   const courseList = props.courses;
 
   const [open, setOpen] = React.useState(props.open);
+  //show filter button group if true
+  const [showFilterTag, setShowFilterTag] = React.useState(false);
+
+  const [showCategory, setShowCategory] = React.useState(null);
+  const [showLevel, setShowLevel] = React.useState(null);
+  const [showModule, setShowModule] = React.useState(null);
+
+  const [instructorList, setInstructorList] = useState([]);
+  const [showInstructor, setShowInstructor] = React.useState(null);
 
   const handleClick = () => {
     setOpen(true);
@@ -119,6 +138,7 @@ export default function CourseViewHome(props) {
 
   useEffect(() => {
     console.log("course list inside CourseViewHome: " + courseList.length);
+    getInstructors();
   }, [courseList]);
 
   function buyCourse(id) {
@@ -141,6 +161,103 @@ export default function CourseViewHome(props) {
       });
   }
 
+  const handleFilterClick = (event) => {
+    if (showFilterTag === true) {
+      setShowFilterTag(false);
+    } else {
+      setShowFilterTag(true);
+    }
+  };
+
+  //for category menu
+  const handleCategoryClick = (event) => {
+    setShowCategory(event.currentTarget);
+  };
+
+  const handleCategoryClose = (event) => {
+    setShowCategory(null);
+  };
+
+  //for level menu
+  const handleLevelClick = (event) => {
+    setShowLevel(event.currentTarget);
+  };
+
+  const handleLevelClose = (event) => {
+    setShowLevel(null);
+  };
+
+  //for module menu
+  const handleModuleClick = (event) => {
+    setShowModule(event.currentTarget);
+  };
+
+  const handleModuleClose = (event) => {
+    setShowModule(null);
+  };
+
+  //for instructor menu
+  const handleInstructorClick = (event) => {
+    setShowInstructor(event.currentTarget);
+  };
+
+  const handleInstructorCose = (event) => {
+    setShowInstructor(null);
+  };
+
+  //get all instructor list from server later it will be list of all featured instructors
+  function getInstructors() {
+    axios
+      .get("instructor/", {
+        headers: {
+          "auth-token": `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoYW5ld2FzYWhtZWRAZ21haWwuY29tIiwicGFzc3dvcmQiOiJQb3RhdG83MjYiLCJpYXQiOjE1OTU4NjA3MzYsImV4cCI6MTU5NTg2NDMzNn0.IRPW-1hioz4LZABZrmtYakjmDwORfKnzIWkwK3DzAXc`,
+        },
+      })
+      .then((res) => {
+        const instructorList = res.data;
+        setInstructorList(instructorList);
+        console.log(
+          "instructor list fetched in courseuploader: " + instructorList
+        );
+      });
+  }
+
+  const categoryList = [
+    {
+      primary: "Guitar",
+      alt: "Guitar",
+      src:
+        "https://i1.pngguru.com/preview/844/649/188/button-ui-2-apple-paid-pro-guitar-icon-png-clipart.jpg",
+    },
+    {
+      primary: "Acoustic Fingerstyle Guitar",
+      alt: "Acoustic Fingerstyle guitar",
+      src: "https://image.flaticon.com/icons/png/512/176/176540.png",
+    },
+    {
+      primary: "Drums",
+      alt: "Drums",
+      src:
+        "https://imageog.flaticon.com/icons/png/512/1803/1803943.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF",
+    },
+    {
+      primary: "Piano/Keyboard",
+      alt: "Piano/Keyboard",
+      src:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxbm59tHjLaXC1joj-gB2Mid9SXnZ7KZTX3A&usqp=CAU",
+    },
+    {
+      primary: "Sound Engineering",
+      alt: "Sound Engineering",
+      src:
+        "https://imageog.flaticon.com/icons/png/512/2198/2198024.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF",
+    },
+  ];
+
+  const levelList = ["Basic", "Intermediate", "Advance", "Pro", "Mastro"];
+
+  const moduleList = ["Level 1", "Level 2", "Level 3", "Level 4", "Level 5"];
+
   return (
     <div>
       <CssBaseline />
@@ -150,31 +267,189 @@ export default function CourseViewHome(props) {
             <div className="App">
               <Navbar />
               <Toolbar style={{ marginBottom: theme.spacing(2) }} />
-              <Grid container direction="row">
+              <Grid container direction="row" spacing={theme.spacing(1)}>
                 <Grid item>
                   <Button
                     color="inherit"
                     variant="outlined"
                     className={classes.Button1}
                     endIcon={<FilterList />}
+                    onClick={handleFilterClick}
                   >
                     Filter by
                   </Button>
                 </Grid>
-                <Grid item>
-                  <ButtonGroup
-                    size="large"
-                    style={{ color: theme.palette.primary.contrastText }}
-                    fullWidth="true"
-                    variant="outlined"
-                    aria-label="large outlined primary button group"
-                  >
-                    <Button>Category</Button>
-                    <Button>Level</Button>
-                    <Button>Module</Button>
-                    <Button>Instructor</Button>
-                  </ButtonGroup>
-                </Grid>
+                {showFilterTag === true && (
+                  <Grid item>
+                    <ButtonGroup
+                      size="large"
+                      color={theme.palette.text.primary}
+                      fullWidth="true"
+                      variant="outlined"
+                      aria-label="large outlined primary button group"
+                    >
+                      <Button
+                        endIcon={<ArrowDropDown fontSize="small" />}
+                        aria-controls="simple-menu"
+                        aria-haspopup="true"
+                        onClick={handleCategoryClick}
+                      >
+                        Category
+                      </Button>
+                      <Menu
+                        id="simple-menu"
+                        anchorEl={showCategory}
+                        keepMounted
+                        open={Boolean(showCategory)}
+                        onClose={handleCategoryClose}
+                        TransitionComponent={Fade}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                          vertical: "bottom",
+                          horizontal: "center",
+                        }}
+                        transformOrigin={{
+                          vertical: "top",
+                          horizontal: "center",
+                        }}
+                        {...props}
+                      >
+                        {categoryList.map((courseCategory, index) => (
+                          <MenuItem
+                            onClick={() => {
+                              navigateToCourse(courseCategory.primary);
+                            }}
+                          >
+                            <ListItemAvatar>
+                              <Avatar
+                                alt={courseCategory.alt}
+                                src={courseCategory.src}
+                              />
+                            </ListItemAvatar>
+                            <ListItemText primary={courseCategory.primary} />
+                          </MenuItem>
+                        ))}
+                      </Menu>
+                      <Button
+                        endIcon={<ArrowDropDown fontSize="small" />}
+                        onClick={handleLevelClick}
+                      >
+                        Level
+                      </Button>
+                      <Menu
+                        id="simple-menu"
+                        anchorEl={showLevel}
+                        keepMounted
+                        open={Boolean(showLevel)}
+                        onClose={handleLevelClose}
+                        TransitionComponent={Fade}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                          vertical: "bottom",
+                          horizontal: "center",
+                        }}
+                        transformOrigin={{
+                          vertical: "top",
+                          horizontal: "center",
+                        }}
+                        {...props}
+                      >
+                        {levelList.map((courseLevel, index) => (
+                          <MenuItem
+                            onClick={() => {
+                              // navigateToCourse(courseCategory.primary);
+                            }}
+                          >
+                            <ListItemText primary={courseLevel} />
+                          </MenuItem>
+                        ))}
+                      </Menu>
+                      <Button
+                        endIcon={<ArrowDropDown fontSize="small" />}
+                        onClick={handleModuleClick}
+                      >
+                        Module
+                      </Button>
+                      <Menu
+                        id="simple-menu"
+                        anchorEl={showModule}
+                        keepMounted
+                        open={Boolean(showModule)}
+                        onClose={handleModuleClose}
+                        TransitionComponent={Fade}
+                        getContentAnchorEl={null}
+                        anchorOrigin={{
+                          vertical: "bottom",
+                          horizontal: "center",
+                        }}
+                        transformOrigin={{
+                          vertical: "top",
+                          horizontal: "center",
+                        }}
+                        {...props}
+                      >
+                        {moduleList.map((courseModule, index) => (
+                          <MenuItem
+                            onClick={() => {
+                              navigateToCourse(courseModule);
+                            }}
+                          >
+                            <ListItemText primary={courseModule} />
+                          </MenuItem>
+                        ))}
+                      </Menu>
+                      <Button
+                        endIcon={<ArrowDropDown fontSize="small" />}
+                        onClick={handleInstructorClick}
+                      >
+                        Instructor
+                      </Button>
+                      <Menu
+                        id="simple-menu"
+                        anchorEl={showInstructor}
+                        keepMounted
+                        open={Boolean(showInstructor)}
+                        onClose={handleInstructorCose}
+                        TransitionComponent={Fade}
+                        getContentAnchorEl={null}
+                        PaperProps={{
+                          style: {
+                            maxHeight: ITEM_HEIGHT * 5.5,
+                            width: "40ch",
+                          },
+                        }}
+                        anchorOrigin={{
+                          vertical: "bottom",
+                          horizontal: "center",
+                        }}
+                        transformOrigin={{
+                          vertical: "top",
+                          horizontal: "center",
+                        }}
+                        {...props}
+                      >
+                        {instructorList.map((courseInstructor, index) => (
+                          <MenuItem
+                            onClick={() => {
+                              // navigateToCourse(courseCategory.primary);
+                            }}
+                          >
+                            <ListItemAvatar>
+                              <Avatar
+                                // alt={courseInstructor.alt}
+                                src={
+                                  "http://162.0.231.67/" +
+                                  courseInstructor.photo
+                                }
+                              />
+                            </ListItemAvatar>
+                            <ListItemText primary={courseInstructor.name} />
+                          </MenuItem>
+                        ))}
+                      </Menu>
+                    </ButtonGroup>
+                  </Grid>
+                )}
               </Grid>
             </div>
           )}
