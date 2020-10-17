c1329e7f582f1dcd6d7cfe56090759660b8629d8
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-10-17 15:38:15 +0600

    CourseView.js | lesson list view added

--- frontend/src/LMS/component/CourseView.js
+++ frontend/src/LMS/component/CourseView.js
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from "react";
 import ScrollToTop from "react-scroll-up";
 import { FiChevronUp } from "react-icons/fi";
-import { PlayArrow } from "@material-ui/icons";
+import { PlayArrow, Lock } from "@material-ui/icons";
+import {
+  makeStyles,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  Avatar,
+} from "@material-ui/core";
 
 //custom components
 import Header from "./HeaderFive.jsx";
@@ -9,7 +18,56 @@ import PageHelmet from "./Helmet.jsx";
 import Footer from "./Footer.jsx";
 import ReactPlayer from "./ReactPlayer";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#191c21",
+  },
+  Icon: {
+    width: theme.spacing(2),
+    height: theme.spacing(2),
+  },
+  ListItemText: {
+    color: "#fff",
+  },
+  ListItem: {
+    "&$selected": {
+      backgroundColor: "red",
+      color: "white",
+    },
+    "&$selected:hover": {
+      backgroundColor: "purple",
+      color: "white",
+    },
+    "&:hover": {
+      backgroundColor: "#f9004d",
+      color: "white",
+    },
+  },
+  Avatar: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    backgroundColor: "#f9004d",
+    marginRight: theme.spacing(2),
+  },
+}));
+
+const lessonName = [
+  "Introduction",
+  "What you'll learn?",
+  "Holding Your 'Guitar' properly",
+  "Navigating the Fretboard",
+  "Strumming Techniques",
+  "Demystifying Chords",
+  "Playing Advanced Chords",
+  "Finding Your Sound",
+  "Technique: Developing Speed",
+  "Improvisation",
+];
+
 export default function CourseView(props) {
+  const classes = useStyles();
   return (
     <div className="active-dark">
       <PageHelmet pageTitle="Course" />
@@ -23,13 +81,13 @@ export default function CourseView(props) {
           <div className="col-lg-12">
             <h3>Intro to Acoustic Guitar</h3>
           </div>
-          <div className="col-lg-9 ">
+          <div className="col-lg-9">
             <ReactPlayer url={"https://www.youtube.com/watch?v=cUxRhesT8gY"} />
           </div>
-          <div className="col-lg-3 bg_color--1">
-            <div className=" pt--10">
-              <h3> Curriculum</h3>
-              <table className="table table-hover table-striped table-dark">
+          <div className="col-lg-3">
+            {/* <div className=" pt--10">
+              <h3> Curriculum</h3> */}
+            {/* <table className="table table-hover table-striped table-dark">
                 <tbody>
                   <tr>
                     <td>
@@ -47,9 +105,25 @@ export default function CourseView(props) {
                   </tr>
                 </tbody>
               </table>
+            </div> */}
+            <div className={classes.root}>
+              <List component="nav" aria-label="main mailbox folders">
+                {lessonName.map((lesson) => (
+                  <ListItem button className={classes.ListItem}>
+                    <Avatar className={classes.Avatar}>
+                      <PlayArrow className={classes.Icon} />
+                    </Avatar>
+                    <ListItemText
+                      primary={lesson}
+                      className={classes.ListItemText}
+                    />
+                  </ListItem>
+                ))}
+              </List>
             </div>
           </div>
         </div>
+        {/* </div> */}
       </div>
       {/* Start Back To Top */}
       <div className="backto-top">
