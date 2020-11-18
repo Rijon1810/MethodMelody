20ac2daa62499bbb9ed007d52046953353000365
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-11-18 09:46:55 +0600

    Config.js | base url changed

--- frontend/src/LMS/api/Config.js
+++ frontend/src/LMS/api/Config.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const instance = axios.create({
-	baseURL: "http://162.0.231.67/api/v1/",
+	baseURL: "http://63.250.33.174/api/v1/",
 });
 
 module.exports = instance;
