8490c027dee485e654a5847840bc5cb842773d23
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2020-12-31 11:13:11 +0600

    Admin.jsx | Stat added to Home

--- frontend/src/LMS/component/Admin.jsx
+++ frontend/src/LMS/component/Admin.jsx
@@ -13,6 +13,7 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import AdminDrawer from "./elements/AdminDrawer.jsx";
 import { makeStyles } from "@material-ui/core/styles";
+import { useSelector, useDispatch } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   content: {
@@ -21,6 +22,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function Admin() {
+  const analytics_data = useSelector(
+    (state) => state.getAnalytics.getAnalytics
+  );
   const classes = useStyles();
   return (
     <React.Fragment>
@@ -29,7 +33,54 @@ function Admin() {
       <main className={classes.content}>
         <Header from="admin" />
 
-        <Breadcrumb from="admin"/>
+        <Breadcrumb from="admin" />
+
+        {/* Start Columns Area  */}
+        <div className="rn-columns-area ptb--50 bg_color--6">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-3 col-md-6 col-12">
+                <div className="single-column custom-color">
+                  <p className="text-white text-center title">Total Course</p>
+                  <h2 className=" text-center text-white">
+                    {analytics_data[0].course}
+                  </h2>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 col-12 mt_sm--30">
+                <div className="single-column custom-color custom-color--1">
+                  <p className="tilte text-white text-center">
+                    Total Instructor
+                  </p>
+                  <h2 className=" text-center text-white">
+                    {analytics_data[0].instructor}
+                  </h2>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 col-12 mt_sm--30 mt_md--30">
+                <div className="single-column custom-color">
+                  <p className="tilte text-white text-center">
+                    Total User
+                  </p>
+                  <h2 className=" text-center text-white">
+                    {analytics_data[0].user}
+                  </h2>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 col-12 mt_sm--30 mt_md--30">
+                <div className="single-column custom-color custom-color--1">
+                  <p className="tilte text-white text-center">
+                    Total Sales
+                  </p>
+                  <h2 className=" text-center text-white">
+                    {analytics_data[0].sold}
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Start Columns Area  */}
 
         {/* Start Back To Top */}
         <div className="backto-top">
