f6669598a68bcc4ff69431a30c0c223166c3f3c4
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-01-03 17:54:44 +0600

    CreateAccount.jsx | CreateAccountForm.jsx

--- frontend/src/LMS/component/CreateAccount.jsx
+++ frontend/src/LMS/component/CreateAccount.jsx
@@ -1,221 +1,58 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getInstructor, postInstructor } from "../../actions/instructorAction";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import PageHelmet from "./Helmet.jsx";
+import Breadcrumb from "./elements/common/Breadcrumb.jsx";
+import {
+  FiCast,
+  FiLayers,
+  FiUsers,
+  FiMonitor,
+  FiChevronUp,
+} from "react-icons/fi";
+import ScrollToTop from "react-scroll-up";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import AdminDrawer from "./elements/AdminDrawer.jsx";
+import { makeStyles } from "@material-ui/core/styles";
+import { useSelector, useDispatch } from "react-redux";
 
-class CreateAccount extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rnName: "",
-      rnEmail: "",
-      rnPassword: "",
-      rnUserType: "",
-      rnBio: "",
-      rnPhoto: "",
-      rnBanner: "",
-    };
-  }
-  render() {
-    return (
-      <div className="contact-form--1">
-        <div className="container">
-          <div className="row row--35 align-items-start">
-            <div className="col-lg-12 order-2 order-lg-1">
-              <div className="section-title text-left theme-gradient">
-                <h4 className="title">Create New User Account</h4>
-              </div>
-              <p className="text-muted">
-                All the fields are rquired for successful account creation.
-              </p>
-              <div className="form-wrapper">
-                <form ref={(el) => (this.form = el)}>
-                  <div className="row">
-                    <div className="col-lg-4">
-                      <label htmlFor="item01">
-                        <input
-                          type="text"
-                          name="name"
-                          id="item01"
-                          value={this.state.rnName}
-                          onChange={(e) => {
-                            this.setState({ rnName: e.target.value });
-                          }}
-                          placeholder="Name *"
-                        />
-                      </label>
-                    </div>
-                    <div className="col-lg-4">
-                      <label htmlFor="item02">
-                        <input
-                          type="text"
-                          name="email"
-                          id="item02"
-                          value={this.state.rnEmail}
-                          onChange={(e) => {
-                            this.setState({ rnEmail: e.target.value });
-                          }}
-                          placeholder="Email *"
-                        />
-                      </label>
-                    </div>
-                    <div className="col-lg-4">
-                      <label htmlFor="item03">
-                        <input
-                          type="text"
-                          name="password"
-                          id="item03"
-                          value={this.state.rnPassword}
-                          onChange={(e) => {
-                            this.setState({ rnPassword: e.target.value });
-                          }}
-                          placeholder="Password *"
-                        />
-                      </label>
-                    </div>
-                  </div>
+import Form from "./CreateAccountForm.jsx";
 
-                  <div className="row">
-                    <div className="col-lg-4">
-                      <div className="form-group">
-                        <label htmlFor="exampleFormControlFile1">
-                          Account Type *
-                        </label>
-                        <label htmlFor="accounttype *">
-                          <select
-                            className="form-control"
-                            name="featured"
-                            onSelect={(e) => {
-                              this.setState({ rnUserType: e.target.value });
-                            }}
-                          >
-                            <option value={true}>Admin</option>
-                            <option value={false}>Content Uploader</option>
-                            <option value={false}>Instructor</option>                         
-                          </select>
-                        </label>
-                      </div>
-                    </div>
-                  </div>
+const useStyles = makeStyles((theme) => ({
+  content: {
+    marginLeft: "240px",
+  },
+}));
 
-                  <div className="row">
-                    <div className="col-lg-4">
-                      <div className="form-group">
-                        <label htmlFor="exampleFormControlFile1">
-                          Thumbnail File
-                        </label>
-                        <label htmlFor="item04">
-                          <input
-                            type="file"
-                            name="photo"
-                            id="item04"
-                            value={this.state.rnPhoto.name}
-                            onChange={(e) => {
-                              let files = e.target.files;
-                              if (files) {
-                                let reader = new FileReader();
-                                reader.onload = (e) => {
-                                  this.setState({
-                                    rnPhoto: e.target.result,
-                                  });
-                                };
-                                reader.readAsDataURL(files[0]);
-                              }
-                            }}
-                            placeholder="Photo *"
-                          />
-                        </label>
-                      </div>
-                    </div>
-                    <div className="col-lg-4">
-                      <div className="form-group">
-                        <label htmlFor="exampleFormControlFile1">
-                          Banner File
-                        </label>
-                        <label htmlFor="item05">
-                          <input
-                            type="file"
-                            name="banner"
-                            id="item05"
-                            value={this.state.rnBanner.name}
-                            onChange={(e) => {
-                              let files = e.target.files;
-                              if (files) {
-                                let reader = new FileReader();
-                                reader.onload = (e) => {
-                                  this.setState({
-                                    rnBanner: e.target.result,
-                                  });
-                                };
-                                reader.readAsDataURL(files[0]);
-                              }
-                            }}
-                            placeholder="Banner *"
-                          />
-                        </label>
-                      </div>
-                    </div>
-                  </div>
+function CreateAccount() {
+  const analytics_data = useSelector(
+    (state) => state.getAnalytics.getAnalytics
+  );
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <PageHelmet pageTitle="Admin Panel" />
 
-                  <label htmlFor="item06">
-                    <textarea
-                      type="text"
-                      id="item06"
-                      name="bio"
-                      value={this.state.rnBio}
-                      onChange={(e) => {
-                        this.setState({ rnBio: e.target.value });
-                      }}
-                      placeholder="Bio *"
-                    />
-                  </label>
-                  <button
-                    className="rn-button-style--2 btn-solid"
-                    type="submit"
-                    value="submit"
-                    name="submit"
-                    id="mc-embedded-subscribe"
-                    onClick={async (event) => {
-                      event.preventDefault();
-                      const body = new FormData(this.form);
-                      toast("Upload started!!! please wait!!");
-                      await this.props.postInstructor(body);
-                      this.props.create_instructor_status.message ===
-                      "Instructor Added Successfully!"
-                        ? toast.info("Instructor Added Successfully!", {
-                            position: "bottom-center",
-                            autoClose: 7000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                          })
-                        : toast("Instructor Add Failed!");
-                    }}
-                  >
-                    Submit
-                  </button>
-                  <ToastContainer
-                    position="bottom-center"
-                    autoClose={7000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                  />
-                </form>
-              </div>
-            </div>
-          </div>
+      <main className={classes.content}>
+        <Header from="admin" />
+
+        <Breadcrumb from="admin" />
+
+        <div className="rn-contact-form-area ptb--50 bg_color--1 pl--60 pr--60">
+          <Form />
         </div>
-      </div>
-    );
-  }
-}
+        {/* Start Back To Top */}
+        <div className="backto-top">
+          <ScrollToTop showUnder={160}>
+            <FiChevronUp />
+          </ScrollToTop>
+        </div>
+        {/* End Back To Top */}
 
+        <Footer />
+      </main>
+
+      <AdminDrawer />
+    </React.Fragment>
+  );
+}
 export default CreateAccount;
