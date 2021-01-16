9b869ae79a802677df942d39dbec053384306fb0
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-01-16 12:41:27 +0600

    add to cart and buy modal added to courseview

--- frontend/src/LMS/component/BuyDialog.js
+++ frontend/src/LMS/component/BuyDialog.js
@@ -1,94 +1,101 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import MuiDialogTitle from '@material-ui/core/DialogTitle';
-import MuiDialogContent from '@material-ui/core/DialogContent';
-import MuiDialogActions from '@material-ui/core/DialogActions';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import CloseIcon from "@material-ui/icons/Close";
+import { Backdrop, Button, Modal, Grid, makeStyles } from "@material-ui/core";
+import Fade from "@material-ui/core/Fade";
 
-const styles = (theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2),
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 2, 4),
+    outline: "none"
   },
-});
+  button: {
+    margin: theme.spacing(1, 0, 0, 0)
+  }
+}));
 
-const DialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props;
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  );
-});
-
-const DialogContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiDialogContent);
-
-const DialogActions = withStyles((theme) => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(1),
-  },
-}))(MuiDialogActions);
-
-export default function BuyDialog() {
+export default function TransitionsModal() {
+  const classes = useStyles();
   const [open, setOpen] = React.useState(false);
 
-  const handleClickOpen = () => {
+  const handleOpen = () => {
     setOpen(true);
   };
+
   const handleClose = () => {
     setOpen(false);
   };
 
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open dialog
-      </Button>
-      <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
-        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
-          Modal title
-        </DialogTitle>
-        <DialogContent dividers>
-          <Typography gutterBottom>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
-            lacus vel augue laoreet rutrum faucibus dolor auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel
-            scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus
-            auctor fringilla.
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={handleClose} color="primary">
-            Save changes
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <button type="button" onClick={handleOpen}>
+        react-transition-group
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
+        disableBackdropClick={true}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <Grid container direction="column">
+              <Grid container direction="row" alignItems="center">
+                <Grid item xs={11}>
+                  {" "}
+                  <p id="transition-modal-description">
+                    Beginner Guitar Course 1
+                  </p>
+                </Grid>
+                <Grid item container xs={1}>
+                  {" "}
+                  <CloseIcon fontsize="small" onClick={handleClose} />
+                </Grid>
+              </Grid>
+              <Grid item>
+                {" "}
+                <h2>2499 BDT</h2>
+              </Grid>
+              <Grid item>
+                <p>for 30 Days Subscription</p>
+              </Grid>
+              <Grid item xs={12}>
+                {" "}
+                <Button
+                  variant="contained"
+                  className={classes.button}
+                  fullWidth={true}
+                >
+                  Add to cart
+                </Button>
+              </Grid>
+              <Grid item>
+                {" "}
+                <Button
+                  variant="contained"
+                  className={classes.button}
+                  fullWidth={true}
+                >
+                  Buy now
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+        </Fade>
+      </Modal>
     </div>
   );
 }
