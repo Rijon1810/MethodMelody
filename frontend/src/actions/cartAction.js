f547378f1ee751629c8d00d09c2d6c26f539955d
Shanewas Ahmed <shanewasahmed@gmail.com>
2021-01-17 18:10:44 +0600

    data for getCart action

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -1,9 +1,9 @@
 import axios from "../LMS/api/Config";
 import { DEL_CART, POST_CART, GET_CART } from "./types";
 
-export const getCart = () => (dispatch) => {
+export const getCart = (data) => (dispatch) => {
   axios
-    .get("cart/", {
+    .get("cart/", data, {
       headers: {
         "auth-token": `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoYW5ld2FzYWhtZWRAZ21haWwuY29tIiwicGFzc3dvcmQiOiJQb3RhdG83MjYiLCJpYXQiOjE1OTU4NjA3MzYsImV4cCI6MTU5NTg2NDMzNn0.IRPW-1hioz4LZABZrmtYakjmDwORfKnzIWkwK3DzAXc`,
       },
