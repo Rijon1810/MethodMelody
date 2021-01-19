30a1d717f6d677cb19f997990d35e1ee37f38841
Shanewas Ahmed <shanewasahmed@gmail.com>
2021-01-19 19:03:32 +0600

    user instructor

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -169,7 +169,7 @@ router
     const address = req.body.address;
     const type = req.body.type;
     const course = req.body.course;
-    const instructor = req.body.instructor;
+    const instructor = Mongoose.Types.ObjectId(req.body.instructor);
     const previousCourse = req.body.previousCourse;
 
     User.find({ email }).then((user) => {
