c9ae111391f4160876f7c8d3c42fb614c2a402a0
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-01-19 15:47:23 +0600

    InstructorPanel.jsx - ui update

--- frontend/src/LMS/component/elements/tab/InstructorPanelTab.jsx
+++ frontend/src/LMS/component/elements/tab/InstructorPanelTab.jsx
@@ -16,8 +16,8 @@ class InstructorPanelTab extends Component {
     super(props);
     this.state = {
       rnName: "",
-      rnPhone: "",
-      rnAddress: "",
+      rnNewPassword: "",
+      rnPassword: "",
     };
   }
   render() {
@@ -32,199 +32,166 @@ class InstructorPanelTab extends Component {
           <Tabs>
             <TabList
               className={`${tabStyle} bg_color--1`}
-              style={{ padding: 50 }}
+              style={{ paddingTop: 50, paddingLeft: 20, paddingBottom: 50 }}
             >
               <Tab>{tab1}</Tab>
 
               <Tab>{tab2}</Tab>
             </TabList>
             <div className="col-lg-8">
-            {/* Start Classroom Area */}
-            <TabPanel>
-              <div className="single-tab-content">
-                <BlogList />
-                <div className="row">
-                  <div className="col-lg-12">
-                    {/* Start Pagination Area */}
-                    <Pagination />
-                    {/* End Pagination Area */}
-                  </div>
-                </div>
-              </div>
-            </TabPanel>
-            {/* End Classroom Area */}
-
-            {/* Start Profile Area */}
-            <TabPanel>
-              <div className="single-tab-content">
-                <div className="row">
-                  {/* Start Info Area */}
-                  <div className="col-lg-4 bg_color--6">
-                    <div className="row">
-                      <div className="col-lg-12">
-                        <div className="thumbnail pt--20">
-                          <a href="#">
-                            <img
-                              className="w-100"
-                              src={
-                                `http://63.250.33.174/` +
-                                this.props.profile.photo
-                              }
-                              alt="Blog Images"
-                            />
-                          </a>
-                        </div>
-                      </div>
-                      <div className="col-lg-12 pt--50">
-                        <Grid
-                          container
-                          direction="row"
-                          alignItems="center"
-                          justify="center"
-                        >
-                          <Grid
-                            item
-                            style={{ marginRight: 10, color: "#f9004c" }}
-                          >
-                            <MailOutline />
-                          </Grid>
-                          <Grid item style={{ color: "#fff" }}>
-                            {this.props.profile.email}
-                          </Grid>
-                        </Grid>
-                      </div>
-                      <div className="col-lg-12 pt--20">
-                        <Grid
-                          container
-                          direction="row"
-                          alignItems="center"
-                          justify="center"
-                        >
-                          <Grid
-                            item
-                            style={{ marginRight: 10, color: "#f9004c" }}
-                          >
-                            <Event />
-                          </Grid>
-                          <Grid item style={{ color: "#fff" }}>
-                            {this.props.profile.dob}
-                          </Grid>
-                        </Grid>
-                      </div>
+              {/* Start Classroom Area */}
+              <TabPanel>
+                <div className="single-tab-content">
+                  <BlogList />
+                  <div className="row">
+                    <div className="col-lg-12">
+                      {/* Start Pagination Area */}
+                      <Pagination />
+                      {/* End Pagination Area */}
                     </div>
                   </div>
-                  {/* End Info Area */}
-                  {/* Start Edit Area */}
-                  <div className="col-lg-8">
-                    <div className="contact-form--1">
-                      <div className="container">
-                        <div className="row row--35 align-items-start">
-                          <div className="col-lg-12 order-2 order-lg-1">
-                            <div className="section-title text-left theme-gradient">
-                              <h4 className="title">My Profile</h4>
-                            </div>
-                            <p className="text-muted">
-                              You can edit below informations of your profile by
-                              hitting the save button.
-                            </p>
-                            <div className="form-wrapper">
-                              <form ref={(el) => (this.form = el)}>
-                                <div className="row">
-                                  <div className="col-lg-12">
-                                    <label
-                                      htmlFor="exampleFormControlFile1"
-                                      className="text-muted"
-                                    >
-                                      Full Name
-                                    </label>
-                                    <label htmlFor="item01">
-                                      <input
-                                        type="text"
-                                        name="name"
-                                        id="item01"
-                                        value={this.state.rnName}
-                                        onChange={(e) => {
-                                          this.setState({
-                                            rnName: e.target.value,
-                                          });
-                                        }}
-                                        placeholder={this.props.profile.name}
-                                        className="text-white"
-                                      />
-                                    </label>
+                </div>
+              </TabPanel>
+              {/* End Classroom Area */}
+
+              {/* Start Profile Area */}
+              <TabPanel style={{ paddingLeft: 20, paddingBottom: 100 }}>
+                <div className="single-tab-content">
+                  <div className="row">
+                    {/* Start Info Area */}
+                    <div className="col-lg-4 bg_color--6">
+                      <div className="row">
+                        <div className="col-lg-12">
+                          <div className="thumbnail pt--20">
+                            <a href="#">
+                              <img
+                                className="w-100"
+                                src={
+                                  `http://63.250.33.174/` +
+                                  this.props.profile.photo
+                                }
+                                // alt="Blog Images"
+                              />
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    {/* End Info Area */}
+                    {/* Start Edit Area */}
+                    <div className="col-lg-8">
+                      <div className="contact-form--1">
+                        <div className="container">
+                          <div className="row row--35 align-items-start">
+                            <div className="col-lg-12 order-2 order-lg-1">
+                              <div className="section-title text-left theme-gradient">
+                                <h4 className="title">My Profile</h4>
+                              </div>
+                              <p className="text-muted">
+                                You can edit below informations of your profile
+                                by hitting the save button.
+                              </p>
+                              <div className="form-wrapper">
+                                <form ref={(el) => (this.form = el)}>
+                                  <div className="row">
+                                    <div className="col-lg-12">
+                                      <label
+                                        htmlFor="exampleFormControlFile1"
+                                        className="text-muted"
+                                      >
+                                        Email
+                                      </label>
+                                      <label htmlFor="item01">
+                                        <input
+                                          type="text"
+                                          name="email"
+                                          id="item01"
+                                          value={this.state.rnEmail}
+                                          onChange={(e) => {
+                                            this.setState({
+                                              rnEmail: e.target.value,
+                                            });
+                                          }}
+                                          placeholder={this.props.profile.email}
+                                          className="text-white"
+                                          disabled={true}
+                                        />
+                                      </label>
+                                    </div>
+
+                                    <div className="col-lg-12">
+                                      <label
+                                        htmlFor="exampleFormControlFile1"
+                                        className="text-muted"
+                                      >
+                                        Old Password
+                                      </label>
+                                      <label htmlFor="item01">
+                                        <input
+                                          type="password"
+                                          name="password"
+                                          id="item02"
+                                          value={this.state.rnPassword}
+                                          onChange={(e) => {
+                                            this.setState({
+                                              rnPassword: e.target.value,
+                                            });
+                                          }}
+                                          placeholder={
+                                            this.props.profile.address
+                                          }
+                                        />
+                                      </label>
+                                    </div>
+
+                                    <div className="col-lg-12">
+                                      <label
+                                        htmlFor="exampleFormControlFile1"
+                                        className="text-muted"
+                                      >
+                                        New Password
+                                      </label>
+                                      <label htmlFor="item01">
+                                        <input
+                                          type="password"
+                                          name="password"
+                                          id="item03"
+                                          value={this.state.rnNewPassword}
+                                          onChange={(e) => {
+                                            this.setState({
+                                              rnNewPassword: e.target.value,
+                                            });
+                                          }}
+                                          placeholder={
+                                            this.props.profile.address
+                                          }
+                                        />
+                                      </label>
+                                    </div>
                                   </div>
 
-                                  <div className="col-lg-12">
-                                    <label
-                                      htmlFor="exampleFormControlFile1"
-                                      className="text-muted"
-                                    >
-                                      Phone
-                                    </label>
-                                    <label htmlFor="item01">
-                                      <input
-                                        type="number"
-                                        name="phone"
-                                        id="item02"
-                                        value={this.state.rnPhone}
-                                        onChange={(e) => {
-                                          this.setState({
-                                            rnPhone: e.target.value,
-                                          });
-                                        }}
-                                        placeholder={this.props.profile.phone}
-                                        className="text-white"
-                                      />
-                                    </label>
-                                  </div>
-
-                                  <div className="col-lg-12">
-                                    <label
-                                      htmlFor="exampleFormControlFile1"
-                                      className="text-muted"
-                                    >
-                                      Address
-                                    </label>
-                                    <label htmlFor="item01">
-                                      <input
-                                        type="text"
-                                        name="address"
-                                        id="item03"
-                                        value={this.state.rnAddress}
-                                        onChange={(e) => {
-                                          this.setState({
-                                            rnAddress: e.target.value,
-                                          });
-                                        }}
-                                        placeholder={this.props.profile.address}
-                                        className="text-white"
-                                      />
-                                    </label>
-                                  </div>
-                                </div>
-
-                                <button
-                                  className="rn-button-style--2 btn-solid"
-                                  type="submit"
-                                  value="submit"
-                                  name="submit"
-                                  id="mc-embedded-subscribe"
-                                  onClick={async (event) => {}}
-                                >
-                                  Save
-                                </button>
-                              </form>
+                                  <button
+                                    className="rn-button-style--2 btn-solid"
+                                    type="submit"
+                                    value="submit"
+                                    name="submit"
+                                    id="mc-embedded-subscribe"
+                                    onClick={async (event) => {}}
+                                  >
+                                    Save
+                                  </button>
+                                </form>
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
+                    {/* End Edit Area */}
                   </div>
-                  {/* End Edit Area */}
                 </div>
-              </div>
-            </TabPanel>
-            {/* End Profile Area */}
+              </TabPanel>
+              {/* End Profile Area */}
             </div>
           </Tabs>
         </div>
