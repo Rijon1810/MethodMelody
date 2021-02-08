ca769baebf2b5f790aca8138f6656c426d1f4fcf
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-02-08 13:06:07 +0600

    CourseView.js | PurchaseHistoryList.jsx

--- frontend/src/LMS/component/CourseView.js
+++ frontend/src/LMS/component/CourseView.js
@@ -256,6 +256,7 @@ export default function CourseView(props) {
                               course: selectedCourse._id,
                             })
                           );
+                          handleClose();
                         }}
                       >
                         Add to cart
