2f8988e1e7c106cb16fb891926c12a269e7ce9c5
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-02-17 16:53:47 +0600

    About.jsx - About added to Footer

--- frontend/src/elements/About.jsx
+++ frontend/src/elements/About.jsx
@@ -4,205 +4,106 @@ import Breadcrumb from "../elements/common/Breadcrumb";
 import CounterOne from "../elements/counters/CounterOne";
 import Testimonial from "../elements/Testimonial";
 import BrandTwo from "../elements/BrandTwo";
-import { FaFacebookF , FaLinkedinIn , FaTwitter } from "react-icons/fa";
-import ScrollToTop from 'react-scroll-up';
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import ScrollToTop from "react-scroll-up";
 import { FiChevronUp } from "react-icons/fi";
-import Header from "../component/header/Header";
-import Footer from "../component/footer/Footer";
+import Header from "../LMS/component/Header.jsx";
+import Footer from "../LMS/component/Footer.jsx";
 
+class About extends Component {
+  render() {
+    let title = "Method Melody",
+      description =
+        "Learning knows no bounds, nor does passion. There is no age limit when it comes to music either, it is an art that only people blessed with talent can express. Method Melody is where all of the above come into harmony and give people the chance to venture into new challenges.";
+    return (
+      <React.Fragment>
+        <PageHelmet pageTitle="About" />
 
-class About extends Component{
-    render(){
-        let title = 'About',
-        description = 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going to use a passage of Lorem Ipsum Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable. If you are going.';
-        return(
-            <React.Fragment>
-                <PageHelmet pageTitle='About' />
+        <Header
+          from="landing"
+        />
+        {/* Start Breadcrump Area */}
+        <Breadcrumb title={"About Method Melody"} />
+        {/* End Breadcrump Area */}
 
-                <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
-                {/* Start Breadcrump Area */}
-                <Breadcrumb title={'About'}   />
-                {/* End Breadcrump Area */}
-
-                {/* Start About Area  */}
-                <div className="rn-about-area ptb--120 bg_color--1">
-                    <div className="rn-about-wrapper">
-                        <div className="container">
-                            <div className="row row--35 align-items-center">
-                                <div className="col-lg-5">
-                                    <div className="thumbnail">
-                                        <img className="w-100" src="/assets/images/about/about-3.jpg" alt="About Images"/>
-                                    </div>
-                                </div>
-                                <div className="col-lg-7">
-                                    <div className="about-inner inner">
-                                        <div className="section-title">
-                                            <h2 className="title">{title}</h2>
-                                            <p className="description">{description}</p>
-                                        </div>
-                                        <div className="row mt--30">
-                                            <div className="col-lg-6 col-md-12 col-sm-12 col-12">
-                                                <div className="about-us-list">
-                                                    <h3 className="title">Who we are</h3>
-                                                    <p>There are many vtions of passages of Lorem Ipsum available, but the majority have suffered.</p>
-                                                </div>
-                                            </div>
-                                            <div className="col-lg-6 col-md-12 col-sm-12 col-12">
-                                                <div className="about-us-list">
-                                                    <h3 className="title">Who we are</h3>
-                                                    <p>There are many vtions of passages of Lorem Ipsum available, but the majority have suffered.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        {/* Start About Area  */}
+        <div className="rn-about-area ptb--120 bg_color--1">
+          <div className="rn-about-wrapper">
+            <div className="container">
+              <div className="row row--35 align-items-center">
+                <div className="col-lg-12">
+                  <div className="about-inner inner">
+                    <div className="section-title">
+                      <h2 className="title">{title}</h2>
                     </div>
-                </div>
-                {/* End About Area  */}
-
-                 {/* Start CounterUp Area */}
-                 <div className="rn-counterup-area pb--120 bg_color--1">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="section-title text-center">
-                                    <h3 className="fontWeight500">Our Fun Facts</h3>
-                                </div>
-                            </div>
+                    <div className="row mt--30">
+                      <div className="col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div className="about-us-list">
+                          <p className="description text-justify">
+                            {description}
+                          </p>
                         </div>
-                        <CounterOne />
+                        <div className="about-us-list">
+                          <p className="text-justify">
+                            Method Melody is an online streaming platform where
+                            big names of the Bangladeshi Music Industry have
+                            come together under one roof to revolutionise the
+                            way of learning to play musical instruments! It
+                            shall be an experience that aspiring artists have
+                            always desired but may never thought were possible,
+                            to learn their musical instrument from famous
+                            artists who have made a significant impact on the
+                            Music Industry.
+                          </p>
+                        </div>
+                        <div className="about-us-list">
+                          <p className="text-justify">
+                            Method Melody hopes to be the answer to making
+                            musical education accessible to all. With renowned
+                            artists as mentors, it aspires to inspire new talent
+                            and enhance existing talent.{" "}
+                          </p>
+                        </div>
+                        <div className="about-us-list">
+                          <p className="text-justify">
+                            Our founders are music enthusiasts who understand
+                            the needs of artists and are actively working
+                            towards giving its subscribers the best value for
+                            their money. We are a small company working towards
+                            a big change. Founded in 2020, amidst a global
+                            pandemic, Method Melody is a completely online
+                            educational platform that ensures the safety of
+                            everyone involved.
+                          </p>
+                        </div>
+                        <div className="about-us-list">
+                          <p className="text-justify">
+                            Our goal is to bridge the path when it comes to
+                            talent reaching its highest potential in the Musical
+                            World.
+                          </p>
+                        </div>
+                      </div>
                     </div>
+                  </div>
                 </div>
-                {/* End CounterUp Area */}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* End About Area  */}
 
-                {/* Start Finding Us Area  */}
-                <div className="rn-finding-us-area rn-finding-us bg_color--1">
-                    <div className="inner">
-                        <div className="content-wrapper">
-                            <div className="content">
-                                <h4 className="theme-gradient">Find Your Work Now</h4>
-                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that.</p>
-                                <a className="rn-btn btn-white" href="/about">Get Started</a>
-                            </div>
-                        </div>
-                        <div className="thumbnail">
-                            <div className="image">
-                                <img src="/assets/images/about/finding-us-01.png" alt="Finding Images"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* End Finding Us Area  */}
+        {/* Start Back To Top */}
+        <div className="backto-top">
+          <ScrollToTop showUnder={160}>
+            <FiChevronUp />
+          </ScrollToTop>
+        </div>
+        {/* End Back To Top */}
 
-                {/* Start Team Area  */}
-                <div className="rn-team-area bg_color--1 ptb--120">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="section-title service-style--3 text-center mb--25">
-                                    <h2 className="title">Skilled Team</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            
-                            {/* Start Single Team  */}
-                            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-                                <div className="team">
-                                    <div className="thumbnail">
-                                        <img className="w-100" src="/assets/images/team/team-01.jpg" alt="Blog Images"/>
-                                    </div>
-                                    <div className="content">
-                                        <h4 className="title">Jone Due</h4>
-                                        <p className="designation">Sr. Web Developer</p>
-                                    </div>
-                                    <ul className="social-icon" >
-                                        <li><a href="https://www.facebook.com/"><FaFacebookF /></a></li>
-                                        <li><a href="http://linkedin.com/"><FaLinkedinIn /></a></li>
-                                        <li><a href="https://twitter.com/"><FaTwitter /></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            {/* End Single Team  */}
-
-                            {/* Start Single Team  */}
-                            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-                                <div className="team">
-                                    <div className="thumbnail">
-                                        <img className="w-100" src="/assets/images/team/team-02.jpg" alt="Blog Images"/>
-                                    </div>
-                                    <div className="content">
-                                        <h4 className="title">BM. Alamin</h4>
-                                        <p className="designation">Sr. Web Developer</p>
-                                    </div>
-                                    <ul className="social-icon" >
-                                        <li><a href="https://www.facebook.com/"><FaFacebookF /></a></li>
-                                        <li><a href="http://linkedin.com/"><FaLinkedinIn /></a></li>
-                                        <li><a href="https://twitter.com/"><FaTwitter /></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            {/* End Single Team  */}
-
-                            {/* Start Single Team  */}
-                            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
-                                <div className="team">
-                                    <div className="thumbnail">
-                                        <img className="w-100" src="/assets/images/team/team-03.jpg" alt="Blog Images"/>
-                                    </div>
-                                    <div className="content">
-                                        <h4 className="title">Jone Due</h4>
-                                        <p className="designation">Sr. Web Developer</p>
-                                    </div>
-                                    <ul className="social-icon" >
-                                        <li><a href="https://www.facebook.com/"><FaFacebookF /></a></li>
-                                        <li><a href="http://linkedin.com/"><FaLinkedinIn /></a></li>
-                                        <li><a href="https://twitter.com/"><FaTwitter /></a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            {/* End Single Team  */}
-
-                        </div>
-                    </div>
-                </div>
-                {/* End Team Area  */}
-
-                {/* Start Testimonial Area */}
-                <div className="rn-testimonial-area bg_color--5 ptb--120">
-                    <div className="container">
-                        <Testimonial />
-                    </div>
-                </div>
-                {/* End Testimonial Area */}
-
-                {/* Start Brand Area */}
-                <div className="rn-brand-area brand-separation bg_color--5 ptb--120">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <BrandTwo />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* End Brand Area */}
-
-                {/* Start Back To Top */}
-                <div className="backto-top">
-                    <ScrollToTop showUnder={160}>
-                        <FiChevronUp />
-                    </ScrollToTop>
-                </div>
-                {/* End Back To Top */}
-                
-                <Footer />
-
-            </React.Fragment>
-        )
-    }
+        <Footer />
+      </React.Fragment>
+    );
+  }
 }
-export default About
+export default About;
