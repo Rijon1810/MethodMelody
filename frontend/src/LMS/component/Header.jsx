f17074676b590ed04be187d3cad524ae014314d1
Ahmed Tanzeer Hossain <tanzeerhossain@zoho.com>
2021-02-20 22:47:41 +0600

    Header.jsx - getByUserType dispatched

--- frontend/src/LMS/component/Header.jsx
+++ frontend/src/LMS/component/Header.jsx
@@ -65,7 +65,7 @@ export default function Header(props) {
       }
       if (user_type === 1) {
         dispatch(getGeneralMessage());
-        // dispatch(getStudentMessage());
+        dispatch(getByType());
       }
     }
 
